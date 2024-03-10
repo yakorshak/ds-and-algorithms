98a93d77c524bfcd25d399a66f5e2fedcde5bde1
Andrii Korshak <yakorshak@gmail.com>
2024-03-10 01:01:43 -0500

    solved 125. Valid Palindrome

---
+++ python/125.py
@@ -0,0 +1,24 @@
+# 125. Valid Palindrome
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        p1 = 0 
+        p2 = len(s) - 1
+        s = s.lower()
+
+        while p1 < p2:
+            if not s[p1].isalnum():
+                p1 += 1
+                continue
+
+            if not s[p2].isalnum():
+                p2 -= 1
+                continue
+
+            if s[p1] == s[p2]:
+                p1 += 1
+                p2 -= 1
+            else:
+                return False
+
+        return True
